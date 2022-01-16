94a2ebb83b0874217df7004b33470443ff2a83ea
Umarrii <77452832+UmarUMahmood@users.noreply.github.com>
2022-01-16 23:04:33 +0000

    get subcount of each subreddit as a list

--- src/app.py
+++ src/app.py
@@ -10,5 +10,26 @@ def get_subreddits(reddit):
     return subreddits
 
 
+def get_subcounts(subreddits):
+
+    subcounts = []
+    sub = {}
+
+    for subreddit in subreddits:
+        sub = {
+            "sub_name": subreddit.display_name.capitalize(),
+            "sub_count": subreddit.subscribers,
+        }
+        subcounts.append(sub)
+
+    # sort the list by name (subreddits starting in lowercase were at the end)
+    subcounts = sorted(subcounts, key=lambda k: k["sub_name"])
+
+    return subcounts
+
+
 subreddits = get_subreddits(reddit)
-print(subreddits)
+subcounts = get_subcounts(subreddits)
+
+for subcount in subcounts:
+    print(subcount)
