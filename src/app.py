eb652466cb4ca7d2fd840feaa30fb61d9c594ce1
Umarrii <77452832+UmarUMahmood@users.noreply.github.com>
2022-01-16 22:59:01 +0000

    get subreddits from multireddit

--- src/app.py
+++ src/app.py
@@ -1,29 +1,14 @@
-import os
-from webbrowser import get
-import auth
+import os, auth
 
 reddit = auth.request()
-
 os.system("clear")
 
 
-def get_subreddits():
-
-    subreddit_list = []
-
+def get_subreddits(reddit):
     # use my multireddit to get the overwatch mains subreddits
     subreddits = reddit.multireddit("umarrii", "owmains").subreddits
-
-    # add the name of each subreddit to the list
-    for subreddit in subreddits:
-        name = subreddit.display_name.capitalize()
-        subreddit_list.append(name)
-
-    # sort the list in alphabetical order
-    subreddit_list.sort()
-
-    return subreddit_list
+    return subreddits
 
 
-subreddits = get_subreddits()
+subreddits = get_subreddits(reddit)
 print(subreddits)
